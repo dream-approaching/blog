c3ad1e32c1cce50877b22c17842c5f119cdeebaa
longzi <891661163@qq.com>
2019-12-17 16:33:50 +0800

    增加antd 按需加载antd

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Button } from 'antd';
 
-export default class Button extends React.Component {
+export default class Buttons extends React.Component {
   handleClick = () => console.log('点击了button');
 
   render() {
     const { children } = this.props;
-    return <div onClick={this.handleClick}>{children}</div>;
+    return (
+      <Button type='primary' onClick={this.handleClick}>
+        {children}
+      </Button>
+    );
   }
 }
--- src/components/button/stories.js
+++ src/components/button/stories.js
@@ -8,10 +8,4 @@ export default {
 
 export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 
-export const emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role='img' aria-label='so cool'>
-      😀 😎 👍
-    </span>
-  </Button>
-);
+export const emoji = () => <Button onClick={action('clicked')}>123</Button>;
