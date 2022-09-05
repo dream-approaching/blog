c6fcb66bdd87333ff2e5037658659ad40b8a2464
zhengjinshou <zhengjinshou@tenwit.com>
2022-09-05 17:16:53 +0800

    fix: 修改publicPath

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   base: '/blog',
-  publicPath: '/blog'
+  publicPath: '/blog/'
   // more config: https://d.umijs.org/config
 });
