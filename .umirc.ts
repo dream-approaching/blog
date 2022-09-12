26bfdd30671350d9d27ce4b8c7446b2647f3d64d
longzi <longzizheng@gmail.com>
2022-09-12 10:22:44 +0800

    antd按需加载

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,16 @@ export default defineConfig({
   logo: '/logo.png',
   outputPath: '/docs-dist/',
   styles: [`.__dumi-default-navbar-logo:not([data-plaintext]) { padding-left: 90px !important; }`],
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
   // base: '/blog',
   // publicPath: '/blog/'
   // more config: https://d.umijs.org/config
